76f3e103a639deff68ecf9da7e04a1a9315ef566
Raysen <raysen@live.cn>
2021-01-30 11:31:28 +0800

    Update to our branding

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,7 +1,7 @@
 # Build fingerprint
 ifeq ($(BUILD_FINGERPRINT),)
-BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
-ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
+BUILD_NUMBER_CUSTOM := $(shell date -u +%m%d%H%M)
+ifneq ($(filter user,$(TARGET_BUILD_VARIANT)),)
 BUILD_SIGNATURE_KEYS := release-keys
 else
 BUILD_SIGNATURE_KEYS := test-keys
@@ -18,26 +18,21 @@ ADDITIONAL_BUILD_PROPERTIES += \
 endif
 
 # Branding
-CUSTOM_BUILD_TYPE ?= UNOFFICIAL
+CUSTOM_BUILD_TYPE ?= beta
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
 CUSTOM_DATE_DAY := $(shell date -u +%d)
-CUSTOM_DATE_HOUR := $(shell date -u +%H)
-CUSTOM_DATE_MINUTE := $(shell date -u +%M)
-CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)
 
-CUSTOM_PLATFORM_VERSION := 11.0
+CUSTOM_VERSION_PROP := 11.0
 
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
 
-CUSTOM_VERSION := PixelExperience-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD)-$(CUSTOM_BUILD_TYPE)
-CUSTOM_VERSION_PROP := eleven
+CUSTOM_VERSION := cuzPixel-$(CUSTOM_VERSION_PROP)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD)-$(CUSTOM_BUILD_TYPE)
 
 ADDITIONAL_BUILD_PROPERTIES += \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
+    org.cuzpixel.version=$(CUSTOM_VERSION_PROP) \
+    org.cuzpixel.version.display=$(CUSTOM_VERSION) \
+    org.cuzpixel.build_date=$(CUSTOM_BUILD_DATE) \
+    org.cuzpixel.build_type=$(CUSTOM_BUILD_TYPE)
